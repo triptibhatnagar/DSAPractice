9be17ab5bda2a9d9514de9ec60b2506418e74a98
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-01-03 21:32:37 +0530

    prevent infinite loop

--- Sorting/CyclicSort.java
+++ Sorting/CyclicSort.java
@@ -47,10 +47,11 @@ public class CyclicSort {
     static void cyclicSort(int arr[]) {
         int i = 0;
         while(i < arr.length) {
-            if(arr[i] != arr[arr[i]-1]) {
+            int correct = arr[i]-1;
+            if(arr[i] != arr[correct]) {
                 int temp = arr[i];
-                arr[i] = arr[arr[i]-1];
-                arr[arr[i]-1] = temp;
+                arr[i] = arr[correct];
+                arr[correct] = temp;
             } else {
                 i++;
             }
