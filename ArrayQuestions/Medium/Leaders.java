1dc774647a52ed19c4fb6812e253000765ff13ef
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-07-19 20:10:31 +0530

    Leaders in an arr

--- ArrayQuestions/Medium/Leaders.java
+++ ArrayQuestions/Medium/Leaders.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Leaders {
     static ArrayList<Integer> leaders_brute(int[] nums) {
-        // T: O(n^2), S: O(n)
+        // T: O(n^2), S: O(1) as space is not used in the logic or to solve
         ArrayList<Integer> list = new ArrayList<>();
         for(int i=0; i<nums.length; i++) {
             boolean greater = true;
@@ -18,8 +19,26 @@ public class Leaders {
         }
         return list;
     }
+    static ArrayList<Integer> leaders_optimal(int[] nums) {
+        // T: O(n+n) // other n for collections.reverse, S: O(1)
+        ArrayList<Integer> list = new ArrayList<>();
+        if(nums.length == 0) {
+            return list;
+        }
+        list.add(nums[nums.length-1]);
+        int max = nums[nums.length-1];
+        for(int i=nums.length-2; i>=0 ; i--) {
+            if(nums[i] > max) {
+                max = nums[i];
+                list.add(nums[i]);
+            }
+        }
+        Collections.reverse(list);
+        return list;
+    }
     public static void main(String[] args) {
         int nums[] = {1, 2, 5, 3, 1, 2};
         System.out.println(leaders_brute(nums));
+        System.out.println(leaders_optimal(nums));
     }
 }
