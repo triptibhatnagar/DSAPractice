fb549ab4ef67d184cec4157aaa60c008f481ad2d
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-07-20 12:31:23 +0530

    inorder-iterative

--- BinaryTrees/Traversals/Inorder.java
+++ BinaryTrees/Traversals/Inorder.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class TreeNode {
     int data;
@@ -21,6 +22,24 @@ public class Inorder {
         nodes.add(root.data);
         recInorder(root.right, nodes);
     }
+    static List<Integer> iterInorder(TreeNode root) {
+        // simulate recursion using stack
+        Stack<TreeNode> st = new Stack<>();
+        List<Integer> nodes = new ArrayList<>();
+        TreeNode node = root;
+        while (true) {
+            if(node != null) {
+                st.push(node);
+                node = node.left;
+            }else {
+                if(st.isEmpty()) break;
+                node = st.pop();
+                nodes.add(node.data);
+                node = node.right;
+            }
+        }
+        return nodes;
+    }
     static List<Integer> initiation(TreeNode root) {
         List<Integer> nodes = new ArrayList<>();
         recInorder(root, nodes);
@@ -30,10 +49,11 @@ public class Inorder {
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);    
         root.left = new TreeNode(4);
-        // root.right = new TreeNode(3);
         root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(2);
-        List<Integer> nodes = initiation(root);
-        System.out.println(nodes);
+        // List<Integer> nodes = initiation(root);
+        // System.out.println(nodes);
+        List<Integer> in = iterInorder(root);
+        System.out.println(in);
     }
 }
