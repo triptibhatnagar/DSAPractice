9e3d6bd31e1718d2974411a84fcffdcdb38d938f
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-07-27 22:45:22 +0530

    pascal-2

---
+++ ArrayQuestions/Medium/PascalTriangleII.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class PascalTriangleII {
+    static int[] pascalTriangleII(int r) {
+        int ans[] = new int[r];
+        ans[0] = 1;
+        for(int i=1; i<r; i++) {
+            ans[i] = (ans[i-1]*(r-i))/i;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int r = 5;
+        System.out.println(Arrays.toString(pascalTriangleII(r)));
+    }
+}
